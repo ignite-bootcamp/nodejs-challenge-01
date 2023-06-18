43672b774e8787fca8b6c82ae03bd305bf57b83e
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-06-18 15:45:45 -0300

    feat: method to convert the request to json

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import http from "http";
 import { routes } from "./routes.js";
+import { json } from "./middlewares/json.js";
 
 const PORT = 3000;
 
-const requestListener = (req, res) => {
+const requestListener = async (req, res) => {
+  await json(req, res);
+
   const route = routes.find((route) => {
     return route.method === req.method && route.path === req.url;
   });
