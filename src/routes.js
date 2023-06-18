4c02c3f5f84af7a0b6ab5a99bd52ad871f060c95
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-06-18 16:00:53 -0300

    feat: created POST /tasks route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,29 @@
-// import { Database } from "./database";
+import { randomUUID } from "node:crypto";
+import { Database } from "./database.js";
 
-// const database = new Database();
+const database = new Database();
 
 export const routes = [
+  {
+    method: "POST",
+    path: "/tasks",
+    handler: (req, res) => {
+      const { body } = req;
+
+      const newTask = {
+        id: randomUUID(),
+        title: body.title,
+        description: body.description,
+        created_at: new Date().toISOString(),
+        completed_at: null,
+        updated_at: new Date().toISOString(),
+      };
+
+      database.insert("tasks", newTask);
+
+      return res.writeHead(201).end();
+    },
+  },
   {
     method: "GET",
     path: "/tasks",
